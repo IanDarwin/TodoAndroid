d9a3dd5cc8820450fc543f876500dcbb72bc3590
Ian <ian@darwinsys.com>
2015-12-10 18:26:29 -0500

    Improve comment

--- src/todomore/android/AndroidTask.java
+++ src/todomore/android/AndroidTask.java
@@ -2,7 +2,7 @@ package todomore.android;
 
 import com.darwinsys.todo.model.Task;
 
-/** Just a TodoMore Task with an added _id, because with a SyncAdapter
+/** Just a TodoMore "Todo Task" with an added _id, because with a SyncAdapter
  * we will need to keep these Task things in both databases.
  * @author Ian Darwin
  */
