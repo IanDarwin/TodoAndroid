1a4b4215f7cc00e87ccc864a69cc294dee2113be
Ian <ian@darwinsys.com>
2015-12-09 23:06:14 -0500

    TodoMore Task with added _id, because SyncAdapter needs to keep these pkey in both databases.

---
+++ src/todomore/android/AndroidTask.java
@@ -0,0 +1,14 @@
+package todomore.android;
+
+import com.darwinsys.todo.model.Task;
+
+/** Just a TodoMore Task with an added _id, because with a SyncAdapter
+ * we will need to keep these Task things in both databases.
+ * @author Ian Darwin
+ */
+public class AndroidTask extends Task {
+
+	private static final long serialVersionUID = 1247383306369798480L;
+
+	long _id;
+}
