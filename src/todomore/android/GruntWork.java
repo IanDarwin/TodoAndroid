a89d507c304efac986fd039c1761a9067eb52b63
Ian <ian@darwinsys.com>
2015-12-10 19:21:39 -0500

    Implement jsonStringToListTask()

--- src/todomore/android/GruntWork.java
+++ src/todomore/android/GruntWork.java
@@ -3,6 +3,11 @@ package todomore.android;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import com.darwinsys.todo.model.Priority;
 import com.darwinsys.todo.model.Task;
 
@@ -58,5 +63,39 @@ public class GruntWork {
 		}
 		return list;
 		}
+
+	/**
+	 * Convert JSON goo like this:
+	 * [{"id":102,"priority":"High","name":"TEST 2",
+	 * "creationDate":{"year":2015,"month":11,"day":1},
+	 * "project":null,"context":{"id":100,"name":"Life"},
+	 * "dueDate":null,"status":"ACTIVE","completedDate":null,
+	 * "modified":1448292432791,"description":"None","complete":false},
+	 * {"id":103,"priority":"Low","name":"Low prio item",
+	 * "creationDate":{"year":2015,"month":10,"day":23},"project":null,
+	 * "context":{"id":80,"name":"Home"},"dueDate":null,"status":"NEW",
+	 * "completedDate":null,"modified":1448312330383,
+	 * "description":"","complete":false}]
+	 * into a proper List<Task>.
+	 */
+	public static List<Task> jsonStringToListTask(String resultStr) {
+		List<Task> ret = new ArrayList<>();
+		try {
+			JSONArray array = (JSONArray) new JSONTokener(resultStr).nextValue();
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject o = array.getJSONObject(i);
+				Task t = new Task();
+				t.setId(o.getLong("id"));
+				t.setPriority(Priority.valueOf(o.getString("priority")));
+				t.setName(o.getString("name"));
+				t.setModified(o.getLong("modified"));
+				// XXX moar!!
+				ret.add(t);
+			}
+		} catch (JSONException e) {
+			throw new RuntimeException("This rudpucker failed to parse! " + e, e);
+		}
+		return ret;
+	}
 }
 
