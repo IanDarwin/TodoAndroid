4b46b6db8e3b715cf7736f3022affcc06c3eccf1
Ian <ian@darwinsys.com>
2016-01-02 22:10:15 -0500

    Add the ListView, try to populate it

--- src/todomore/android/MainActivity.java
+++ src/todomore/android/MainActivity.java
@@ -51,169 +51,185 @@ public class MainActivity extends Activity {
 	private ListView mListView;
 	private TaskDao mDao;
 	private int ACTIVITY_ID_LOGIN;
-	private SharedPreferences mPrefs;
+	private static SharedPreferences mPrefs;
 	// Keys for mPrefs lookups
-	private String KEY_USERNAME, KEY_PASSWORD, KEY_HOSTNAME, KEY_HOSTPORT, KEY_HOSTPATH;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        addTF = (EditText) findViewById(R.id.addTF);
-        prioSpinner = (Spinner) findViewById(R.id.prioSpinner);
-        mDao = new TaskDao(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.priorities_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        KEY_USERNAME = getString(R.string.key_username);
-        KEY_PASSWORD = getString(R.string.key_password);
-        KEY_HOSTNAME = getString(R.string.key_hostname);
-        KEY_HOSTPORT = getString(R.string.key_hostport);
-        KEY_HOSTPATH = getString(R.string.key_hostpath);
+	private static String KEY_USERNAME, KEY_PASSWORD, KEY_HOSTNAME, KEY_HOSTPORT, KEY_HOSTPATH;
 
-        prioSpinner.setAdapter(adapter);
-        
-        ensureLogin();
-    }
-    
-    @Override
-    protected void onRestart() {
-    	super.onRestart();
-    	addTF.requestFocus();
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	getMenuInflater().inflate(R.menu.main_menu, menu);
-    	return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch(item.getItemId()) {
-    	case R.id.settings_menuitem:
-    		startActivity(new Intent(this, PrefsActivity.class));
-    		return true;
-    	case R.id.help_menuitem:
-    		Toast.makeText(this, "Help not written yet", Toast.LENGTH_SHORT).show();
-    		return true;
-    	default:
-    		return super.onOptionsItemSelected(item);
-    	}
-    }
-    
-    /** Called from the View when the Add button is pressed */
-    public void addItem(View v) {
-    	String name = addTF.getText().toString();
-    	Log.d(TAG, "addItem: " + name);
-    	if (name == null || name.isEmpty()) {
-    		Toast.makeText(this, "Text required!", Toast.LENGTH_SHORT).show();
-    		return;
-    	}
-    	ensureLogin();
-    	
-    	// Do the work here! Save to local DB before sending to the server...
-    	AndroidTask t = new AndroidTask();
-    	t.setName(addTF.getText().toString());
-    	t.setPriority(Priority.values()[prioSpinner.getSelectedItemPosition()]);
-    	t.setModified(System.currentTimeMillis());
-    	
-    	long _id = mDao.insert(t);
-    	t._id = _id;
-    	Toast.makeText(this, "Saved locally", Toast.LENGTH_SHORT).show();
-    	
-    	// XXX Send to server now; later, will just trigger sync?
-    	new SendObjectAsyncTask().execute(t);
-    	
-    	// If we get here, remove text from TF so task doesn't get added twice
-    	addTF.setText("");
-    	
-    	Toast.makeText(this, "Sync request send", Toast.LENGTH_SHORT).show();
-    	new GetListAsyncTask().execute();
-    }
-    
-    private boolean isLoginOK;
-    
-    /** 
-     * If the user hasn't provided username and password, press them for credentials
-     */
-    private void ensureLogin() {
-    	if (!isLoginOK) {
-    		String userName = mPrefs.getString(KEY_USERNAME, null);
-    		String password = mPrefs.getString(KEY_PASSWORD, null);
-    		if (userName == null || userName.isEmpty() ||
-    				password == null || password.isEmpty()) {
-    			startActivityForResult(new Intent(this, PrefsActivity.class), ACTIVITY_ID_LOGIN);
-    		}
-    	}
-    }
-	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		addTF = (EditText) findViewById(R.id.addTF);
+		prioSpinner = (Spinner) findViewById(R.id.prioSpinner);
+		mListView = (ListView) findViewById(R.id.listView);
+		mDao = new TaskDao(this);
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.priorities_array,
+				android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		prioSpinner.setAdapter(adapter);
+
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		KEY_USERNAME = getString(R.string.key_username);
+		KEY_PASSWORD = getString(R.string.key_password);
+		KEY_HOSTNAME = getString(R.string.key_hostname);
+		KEY_HOSTPORT = getString(R.string.key_hostport);
+		KEY_HOSTPATH = getString(R.string.key_hostpath);
+
+
+		ensureLogin();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		addTF.requestFocus();
+		if (isLoginOK) {
+			new GetListAsyncTask().execute();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings_menuitem:
+			startActivity(new Intent(this, PrefsActivity.class));
+			return true;
+		case R.id.help_menuitem:
+			Toast.makeText(this, "Help not written yet", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/** Called from the View when the Add button is pressed */
+	public void addItem(View v) {
+		String name = addTF.getText().toString();
+		Log.d(TAG, "addItem: " + name);
+		if (name == null || name.isEmpty()) {
+			Toast.makeText(this, "Text required!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		ensureLogin();
+
+		// Do the work here! Save to local DB before sending to the server...
+		AndroidTask t = new AndroidTask();
+		t.setName(addTF.getText().toString());
+		t.setPriority(Priority.values()[prioSpinner.getSelectedItemPosition()]);
+		t.setModified(System.currentTimeMillis());
+
+		long _id = mDao.insert(t);
+		t._id = _id;
+		Toast.makeText(this, "Saved locally", Toast.LENGTH_SHORT).show();
+
+		// XXX Send to server now; later, will just trigger sync?
+		new SendObjectAsyncTask().execute(t);
+
+		// If we get here, remove text from TF so task doesn't get added twice
+		addTF.setText("");
+
+		Toast.makeText(this, "Sync request send", Toast.LENGTH_SHORT).show();
+		new GetListAsyncTask().execute();
+	}
+
+	private boolean isLoginOK;
+
+	/**
+	 * If the user hasn't provided username and password, press them for
+	 * credentials
+	 */
+	private void ensureLogin() {
+		if (!isLoginOK) {
+			String userName = mPrefs.getString(KEY_USERNAME, null);
+			String password = mPrefs.getString(KEY_PASSWORD, null);
+			if (userName == null || userName.isEmpty() || password == null || password.isEmpty()) {
+				startActivityForResult(new Intent(this, PrefsActivity.class), ACTIVITY_ID_LOGIN);
+			}
+		}
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode != ACTIVITY_ID_LOGIN) {
 			super.onActivityResult(requestCode, resultCode, data);
 			return;
 		}
-		// Not secure enough for general use, but since the eserver side will actually
-		// validate the credentials, this is OK for use in this app.
+		// Not secure enough for general use, but since the server side will
+		// actually validate the credentials, this is OK for use in this app.
 		if (resultCode == RESULT_OK) {
 			isLoginOK = true;
 		}
 	}
-	
-	private String getUserName() { return mPrefs.getString(KEY_USERNAME, null); }
-	private String getPassword() { return mPrefs.getString(KEY_PASSWORD, null); }
-	
-	/** Should be used by all REST tasks */
-	protected URI makeRestUri(SharedPreferences prefs) {
+
+	private static String getUserName() {
+		return mPrefs.getString(KEY_USERNAME, null);
+	}
+
+	private static String getPassword() {
+		return mPrefs.getString(KEY_PASSWORD, null);
+	}
+
+	protected static URI makeSendUri(SharedPreferences prefs) {
 		try {
-			return new URI(String.format("http://%s:%d/%s/todo/%s/tasks", 
-					mPrefs.getString(KEY_HOSTNAME, null),
-					Integer.parseInt(mPrefs.getString(KEY_HOSTPORT, "80")),
-					mPrefs.getString(KEY_HOSTPATH, "/"), getUserName()));
+			String pathStr = prefs.getString(KEY_HOSTPATH, "/");
+			return new URI(String.format("http://%s:%d/%s/new/tasks", 
+					prefs.getString(KEY_HOSTNAME, null),
+					Integer.parseInt(prefs.getString(KEY_HOSTPORT, "80")),
+					pathStr.startsWith("/") ? pathStr.substring(1) : pathStr));
 		} catch (URISyntaxException e) {
 			throw new RuntimeException("Failed to create path! " + e, e);
 		}
 	}
-    
-	public class SendObjectAsyncTask extends AsyncTask<Task, Void, Long>{
+	protected static URI makeListUri(SharedPreferences prefs) {
+		try {
+			String pathStr = prefs.getString(KEY_HOSTPATH, "/");
+			return new URI(String.format("http://%s:%d/%s/%s/tasks", prefs.getString(KEY_HOSTNAME, null),
+					Integer.parseInt(prefs.getString(KEY_HOSTPORT, "80")),
+					pathStr.startsWith("/") ? pathStr.substring(1) : pathStr, getUserName()));
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Failed to create path! " + e, e);
+		}
+	}
+
+	public class SendObjectAsyncTask extends AsyncTask<Task, Void, Long> {
 		final ObjectMapper jacksonMapper = new ObjectMapper();
 
 		@Override
 		protected Long doInBackground(Task... params) {
 			ensureLogin();
-    		
+
 			// The number shalle be one...
 			Task t = params[0];
 			Log.d(TAG, "Starting TODO send of task " + t + " for user " + getUserName());
-			
+
 			HttpClient client = new DefaultHttpClient();
-			Credentials creds = new UsernamePasswordCredentials(getUserName(), getPassword());        
-			((AbstractHttpClient)client).getCredentialsProvider()
-				.setCredentials(new AuthScope(
-						mPrefs.getString(KEY_HOSTNAME, "10.0.2.2"), 
-						Integer.parseInt(mPrefs.getString(KEY_HOSTPORT, "80"))),
-						creds); 
+			Credentials creds = new UsernamePasswordCredentials(getUserName(), getPassword());
+			((AbstractHttpClient) client).getCredentialsProvider()
+					.setCredentials(new AuthScope(mPrefs.getString(KEY_HOSTNAME, "10.0.2.2"),
+							Integer.parseInt(mPrefs.getString(KEY_HOSTPORT, "80"))), creds);
 			try {
-				final URI postUri = makeRestUri(mPrefs);
-			
+				final URI postUri = makeSendUri(mPrefs);
+
 				// Send a POST request with to upload this Task
 				Log.d(TAG, "Connecting to server for " + postUri);
 
 				HttpPost postAccessor = new HttpPost();
 				postAccessor.setURI(postUri);
 				postAccessor.addHeader("Content-Type", "application/json");
-				postAccessor.addHeader("Accept", "application/json");
-				
+				postAccessor.addHeader("Accept", "text/plain");
+
 				final ObjectWriter w = jacksonMapper.writer();
-				String json = w
-				  .with(SerializationFeature.INDENT_OUTPUT)
-				  .without(SerializationFeature.WRAP_EXCEPTIONS)
-				  .writeValueAsString(t);
+				String json = w.with(SerializationFeature.INDENT_OUTPUT).without(SerializationFeature.WRAP_EXCEPTIONS)
+						.writeValueAsString(t);
 
 				postAccessor.setEntity(new StringEntity(json));
 
@@ -224,15 +240,15 @@ public class MainActivity extends Activity {
 				HttpEntity postResults = response.getEntity();
 				final String resultStr = EntityUtils.toString(postResults);
 				Log.d(TAG, "Result from SEND: " + resultStr);
-				
-				// it actually sends the URL of the new ID
+
+				// on success it should send us the URL of the new ID, we need to save the remote id in our db
 				Uri resultUri = Uri.parse(resultStr);
 				long id = ContentUris.parseId(resultUri);
 				t.setId(id);
 				if (!mDao.update(t)) {
 					Log.e(TAG, "FAILED TO UPDATE");
 				}
-				Log.d(TAG, "UPDATED " + t + ", new _ID = " + t.getId());
+				Log.d(TAG, "UPDATED " + t + ", new Remote ID = " + t.getId());
 				return id;
 			} catch (RuntimeException e) { // Avoid bloating the stack trace
 				throw e;
@@ -242,31 +258,29 @@ public class MainActivity extends Activity {
 		}
 
 	}
-	
-	public class GetListAsyncTask extends AsyncTask<Void, Void, List<Task>>{
+
+	public class GetListAsyncTask extends AsyncTask<Void, Void, List<Task>> {
 		final ObjectMapper jacksonMapper = new ObjectMapper();
 
 		@Override
 		protected List<Task> doInBackground(Void... params) {
 			Log.d(TAG, "Starting TODO list-fetch for " + getUserName());
-			
+
 			HttpClient client = new DefaultHttpClient();
-			Credentials creds = new UsernamePasswordCredentials(getUserName(), getPassword());        
-			((AbstractHttpClient)client).getCredentialsProvider()
-			.setCredentials(new AuthScope(
-					mPrefs.getString(KEY_HOSTNAME, "10.0.2.2"), 
-					Integer.parseInt(mPrefs.getString(KEY_HOSTPORT, "80"))),
-					creds);  
+			Credentials creds = new UsernamePasswordCredentials(getUserName(), getPassword());
+			((AbstractHttpClient) client).getCredentialsProvider()
+					.setCredentials(new AuthScope(mPrefs.getString(KEY_HOSTNAME, "10.0.2.2"),
+							Integer.parseInt(mPrefs.getString(KEY_HOSTPORT, "80"))), creds);
 			try {
-				final URI postUri = makeRestUri(mPrefs);
-			
+				final URI postUri = makeListUri(mPrefs);
+
 				// Send a GET request with to list the Tasks
 				Log.d(TAG, "Connecting to server for " + postUri);
 
 				HttpGet httpAccessor = new HttpGet();
 				httpAccessor.setURI(postUri);
 				httpAccessor.addHeader("Accept", "application/json");
-				
+
 				// INVOKE
 				HttpResponse response = client.execute(httpAccessor);
 
@@ -277,12 +291,15 @@ public class MainActivity extends Activity {
 
 				// Service sends the list of Tasks in JSON
 				List<Task> list = GruntWork.jsonStringToListTask(resultStr);
+				Log.d(TAG, "LIST SIZE = " + list.size());
 				return list;
+			} catch (RuntimeException e) { // Avoid bloating the stack trace
+				throw e;
 			} catch (Exception e) {
 				throw new RuntimeException("Get List failed!" + e, e);
 			}
 		}
-		
+
 		@Override
 		protected void onPostExecute(List<Task> result) {
 			ListAdapter adapter = new ArrayAdapter<Task>(MainActivity.this, 0);
