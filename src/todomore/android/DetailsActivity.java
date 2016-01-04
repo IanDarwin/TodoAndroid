dc8ad0e8d6a990aad614cd5110221743ffab9f2f
Ian <ian@darwinsys.com>
2016-01-04 17:20:56 -0500

    Make metawidget object a field so enableEditing() can find it

--- src/todomore/android/DetailsActivity.java
+++ src/todomore/android/DetailsActivity.java
@@ -6,37 +6,49 @@ import com.darwinsys.todo.model.Task;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+/** 
+ * An Activity that shows all the Details of one Task, using MetaWidget.
+ * It starts in display mode, but an Edit button makes it editable.
+ * @author Ian Darwin
+ */
 public class DetailsActivity extends Activity {
 
+	private static final String TAG = DetailsActivity.class.getSimpleName();
 	private Task mTask;
+	private AndroidMetawidget mMetawidget;
 
 	public void onCreate(Bundle saved) {
 		super.onCreate(saved);
 		setContentView(R.layout.details_activity);
 
 		long id = getIntent().getLongExtra("taskId", -1);
-
+		Log.d(TAG, "_id = " + id);
 		if (id != -1) {
 			mTask = ((TodoMoreApplication) getApplication()).getTaskDao().findById(id);
 		} else {
 			mTask = new AndroidTask();
 		}
 
-		final AndroidMetawidget metawidget = 
-			(AndroidMetawidget) findViewById(R.id.metawidget);
-        metawidget.setToInspect(mTask);
-        if ( id == -1) {
-            metawidget.setReadOnly(false);
+		mMetawidget = (AndroidMetawidget) findViewById(R.id.metawidget);
+        mMetawidget.setToInspect(mTask);
+        if (id == -1) {
+            mMetawidget.setReadOnly(false);
         }
 
-        if (!metawidget.isReadOnly()) {
-            metawidget.buildWidgets();
-            View view = metawidget.findViewWithTag("name");
+        if (!mMetawidget.isReadOnly()) {
+            mMetawidget.buildWidgets();
+            View view = mMetawidget.findViewWithTag("name");
             view.setFocusable(true);
             view.setFocusableInTouchMode(true);
             view.requestFocus();
         }
+	}
+	
+	public void enableEditing(View v) {
+		Log.d(TAG, "enableEditing");
+		mMetawidget.setReadOnly(false);
 	}
 }
