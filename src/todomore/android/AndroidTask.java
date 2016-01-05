1762799ba5c76bbf52c88654fb2022f1c42c3561
Ian <ian@darwinsys.com>
2016-01-04 20:39:35 -0500

    @UiHidden on _id

--- src/todomore/android/AndroidTask.java
+++ src/todomore/android/AndroidTask.java
@@ -1,8 +1,11 @@
 package todomore.android;
 
+import org.metawidget.inspector.annotation.UiHidden;
+
 import com.darwinsys.todo.model.Task;
 
-/** Just a TodoMore "Todo Task" with an added _id, because with a SyncAdapter
+/** 
+ * A TodoMore "Todo Task" with an added _id, because with a SyncAdapter
  * we will need to keep these Task things in both databases.
  * @author Ian Darwin
  */
@@ -10,8 +13,9 @@ public class AndroidTask extends Task {
 
 	private static final long serialVersionUID = 1247383306369798480L;
 
-	long _id;
+	private long _id;
 
+	@UiHidden
 	public long get_Id() {
 		return _id;
 	}
