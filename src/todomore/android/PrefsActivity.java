28f2c31a04d06c8f4c4912cff1600f8d13d6debb
Ian <ian@darwinsys.com>
2016-03-28 22:46:45 -0400

    Big diff: adds Test function which works, although...
    It tests using a URLConnection, unlike the mainline Sync which uses
    the dying HttpClient code, and will have to be rewritten RSN anyway.

--- src/todomore/android/PrefsActivity.java
+++ src/todomore/android/PrefsActivity.java
@@ -1,12 +1,27 @@
 package todomore.android;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import todomore.android.sync.TodoSyncAdapter;
 
 
 /**
@@ -18,12 +33,15 @@ public class PrefsActivity extends Activity {
 	
     static final int DEFAULT_MINUTES_INTERVAL = 60;
 
+	private SharedPreferences mPrefs;
+
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         // Set up defaults before creating PrefsFragment
         PreferenceManager.setDefaultValues(this, R.layout.prefs, false);
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         
         getFragmentManager().beginTransaction()
         	.replace(android.R.id.content, new PrefsFragment())
@@ -49,10 +67,6 @@ public class PrefsActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-    
-    private void testNetworkSetup() {
-		Toast.makeText(this, "Test not written yet", Toast.LENGTH_SHORT).show();
-	}
 
 	public static class PrefsFragment extends PreferenceFragment {
     	public void onCreate(Bundle savedInstanceState) {
@@ -60,7 +74,55 @@ public class PrefsActivity extends Activity {
     		addPreferencesFromResource(R.layout.prefs);
     	}
     }
-}
+	
+    private void testNetworkSetup() {
+		new AsyncTask<Void, String, Boolean>() {
+		
+			@Override
+			public void onProgressUpdate(String... values) {
+				Toast.makeText(PrefsActivity.this, values[0], Toast.LENGTH_LONG).show();
+			};
+			
+			@Override
+			protected Boolean doInBackground(Void... args) {
+				try {
+					URL url = TodoSyncAdapter.makeRequestUrl(mPrefs, "/status");
+					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+					conn.addRequestProperty("Authorization", makeBasicAuthString(mPrefs));
+					InputStream is = conn.getInputStream();
+					publishProgress("Connection status code " + conn.getResponseCode());
+					String response = read(is);
+					JSONObject result = new JSONObject(response);
+					String status = "OK! " + result.getString("status");
+					publishProgress(status);
+					return Boolean.TRUE;
+				} catch (IOException | JSONException e) {
+					final String message = "REST error: " + e;
+					Log.e(TAG, message, e);
+					publishProgress(message);
+					return Boolean.FALSE;
+				}
+			}
+		}.execute();
+	}
 
+	private String makeBasicAuthString(SharedPreferences mPrefs) {
+		String userName = mPrefs.getString(MainActivity.KEY_USERNAME, "nonesuch");
+        String password = mPrefs.getString(MainActivity.KEY_PASSWORD, "don't use this");
+        String authInfo = "Basic " + Base64.encodeToString(
+            (userName + ":" + password).getBytes(), 0);
+        return authInfo;
+	}
+	
+    private static String read(InputStream in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(in), 1000);
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
+            sb.append(line);
+        }
+        in.close();
+        return sb.toString();
+    }
+}
 
 
